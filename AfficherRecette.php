aa8b546f91eb8cadddd239453da53fc76495afbd
anyhony58310 <76625426+anyhony58310@users.noreply.github.com>
2023-09-12 21:35:12 +0200

    Les fonctions

--- AfficherRecette.php
+++ AfficherRecette.php
@@ -1,8 +1,17 @@
 <?php
+function rechercherRecetteParNom($recipes, $nom) {
+    foreach ($recipes as $recipe) {
+        if (isset($recipe['name']) && strtolower($recipe['name']) === strtolower($nom)) {
+            return $recipe;
+        }
+    }
+    return null; // Aucune recette trouvée
+}
+
 $recipes = [
     [
         'name' => 'Cassoulet',
-        'description' => 'Je ne sais pas donc je met n improte quoi',
+        'description' => 'Je ne sais pas donc je mets n\'importe quoi',
         'email' => 'mickael.andrieu@exemple.com',
         'isVegetarian' => true,
     ],
@@ -10,9 +19,17 @@ $recipes = [
         'name' => 'Couscous',
         'description' => '[...]',
         'email' => 'mickael.andrieu@exemple.com',
-        'isVegetarian' => false,
+        'isVegetarian' => true,
     ],
 ];
+
+$nomRecherche = ''; // Initialisez la variable de recherche
+
+// Vérifiez si le formulaire de recherche a été soumis
+if (isset($_POST['nomRecherche'])) {
+    $nomRecherche = $_POST['nomRecherche']; // Obtenez le nom recherché
+    $recetteCherchee = rechercherRecetteParNom($recipes, $nomRecherche);
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +37,14 @@ $recipes = [
 <head>
     <title>Affichage des recettes</title>
     <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
         ul {
             list-style-type: none;
             padding: 0;
@@ -39,29 +64,39 @@ $recipes = [
         strong {
             color: #007bff;
         }
-
-        body {
-    		display: flex;
-    		justify-content: center;
-    		align-items: center;
-    		height: 100vh;
-    		margin: 0;
-		}
     </style>
 </head>
 <body>
-<ul>
-    <?php foreach ($recipes as $recipe): ?>
-        <?php if ($recipe['isVegetarian']): ?>
-            <li>
-                <strong>Nom de la recette:</strong> <?php echo $recipe['name']; ?><br>
-                <strong>Description:</strong> <?php echo $recipe['description']; ?><br>
-                <strong>Email de l'auteur:</strong> <?php echo $recipe['email']; ?><br>
-                <strong>Vegetarienne:</strong> Oui
-            </li>
+<div>
+    <h1>Affichage des recettes</h1>
+    <form method="post">
+        <label for="nomRecherche">Rechercher une recette par nom : </label>
+        <input type="text" id="nomRecherche" name="nomRecherche">
+        <input type="submit" value="Rechercher">
+    </form>
+    <ul>
+        <?php if (isset($recetteCherchee)): ?>
+            <?php if ($recetteCherchee !== null): ?>
+                <li>
+                    <strong>Nom de la recette:</strong> <?php echo $recetteCherchee['name']; ?><br>
+                    <strong>Description:</strong> <?php echo $recetteCherchee['description']; ?><br>
+                    <strong>Email de l'auteur:</strong> <?php echo $recetteCherchee['email']; ?><br>
+                    <strong>Vegetarienne:</strong> <?php echo $recetteCherchee['isVegetarian'] ? 'Oui' : 'Non'; ?>
+                </li>
+            <?php else: ?>
+                <li>Aucune recette trouvée pour "<?php echo $nomRecherche; ?>"</li>
+            <?php endif; ?>
+        <?php else: ?>
+            <?php foreach ($recipes as $recipe): ?>
+                <li>
+                    <strong>Nom de la recette:</strong> <?php echo $recipe['name']; ?><br>
+                    <strong>Description:</strong> <?php echo $recipe['description']; ?><br>
+                    <strong>Email de l'auteur:</strong> <?php echo $recipe['email']; ?><br>
+                    <strong>Vegetarienne:</strong> <?php echo $recipe['isVegetarian'] ? 'Oui' : 'Non'; ?>
+                </li>
+            <?php endforeach; ?>
         <?php endif; ?>
-    <?php endforeach; ?>
-</ul>
+    </ul>
+</div>
 </body>
 </html>
-
