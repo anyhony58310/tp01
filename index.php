2c896946baa728287d5920d8fe0db59e0bcc5714
anyhony58310 <76625426+anyhony58310@users.noreply.github.com>
2023-10-17 14:36:26 +0200

    Connecter PHP à MySQL avec PDO

--- index.php
+++ index.php
@@ -1,68 +1,45 @@
+<?php session_start(); // $_SESSION ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon site de recettes</title>
-        <link rel="stylesheet" type="text/css" href="styles.css">
-    </head>
- 
-    <body>
- 
-    <!-- L'en-tête -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de Recettes - Page d'accueil</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+
+    <!-- Navigation -->
+    <?php include_once('header.php'); ?>
+
+    <!-- Inclusion des fichiers utilitaires -->
+    <?php 
+        include_once('variables.php');
+        include_once('functions.php');
+    ?>
+
+    <!-- Inclusion du formulaire de connexion -->
+    <?php include_once('login.php'); ?>
     
-    <header>
-        <!-- Le menu -->
-    
-        <?php include('header.php'); ?>
-       
-    </header>
-    
-    <!-- Le corps -->
-    
-    <div id="corps">
-        <h1>Mon site de recettes</h1>
-                
-            <p>
-                Bienvenue sur mon site de recettes !
-                <br>
-                <?php include('function.php'); ?>
-            </p>
-        </div>
-    
-    
-        <div>
-    <h1>Affichage des recettes</h1>
-    <form method="post">
-        <label for="nomRecherche">Tape le nom de ta recette pour en savoir +! : </label>
-        <input type="text" id="nomRecherche" name="nomRecherche">
-        <input type="submit" value="Rechercher">
-    </form>
-    <ul>
-        <?php if (isset($recetteCherchee)): ?>
-            <?php if ($recetteCherchee !== null): ?>
-                <li>
-                    <strong>Nom de la recette:</strong> <?php echo $recetteCherchee['name']; ?><br>
-                    <strong>Auteur:</strong> <?php echo $recetteCherchee['auteur']; ?><br>
-                    <strong>Description:</strong> <?php echo $recetteCherchee['description']; ?><br>
-                    <strong>Email de l'auteur:</strong> <?php echo $recetteCherchee['email']; ?><br>
-                    <strong>Est complète:</strong> <?php echo $recetteCherchee['isEnabled'] ? 'Oui' : 'Non'; ?>
-                </li>
-            <?php else: ?>
-                <li>Aucune recette trouvée pour "<?php echo $nomRecherche; ?>"</li>
-            <?php endif; ?>
-        <?php else: ?>
-            <?php foreach ($recipes as $recipe): ?>
-                <li>
-                    <strong>Nom de la recette:</strong> <?php echo $recipe['name']; ?><br>
-                    <strong>Est complète:</strong> <?php echo $recipe['isEnabled'] ? 'Oui' : 'Non'; ?>
-                </li>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </ul>
-</div>
-<footer id="pied_de_page">
-        <?php include('footer.php'); ?>
-    </footer>
-    
-    </body>
+    <h1>Site de Recettes !</h1>
+
+    <!-- Si l'utilisateur existe, on affiche les recettes -->
+    <?php if(isset($_SESSION['LOGGED_USER'])): ?>
+        <?php foreach(getRecipes($recipes, 5) as $recipe) : ?>
+            <article>
+                <h3><?php echo $recipe['title']; ?></h3>
+                <div><?php echo $recipe['recipe']; ?></div>
+                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+            </article>
+        <?php endforeach ?>
+    <?php endif; ?>
+    </div>
+
+    <?php include_once('footer.php'); ?>
+</body>
 </html>
