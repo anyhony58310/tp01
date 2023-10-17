8b9aa8c0a33ded55db4237df26357c1a8719d799
anyhony58310 <76625426+anyhony58310@users.noreply.github.com>
2023-10-17 15:06:20 +0200

    Effectuer une première requete SQL

--- mysql.php
+++ mysql.php
@@ -4,6 +4,7 @@ const MYSQL_PORT = 3306;
 const MYSQL_NAME = 'my_recipes';
 const MYSQL_USER = 'root';
 const MYSQL_PASSWORD = 'root';
+
 try {
     $db = new PDO(
         sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8',
@@ -12,7 +13,24 @@ try {
         MYSQL_PASSWORD
     );
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(Exception $exception) {
-    die('Erreur : '.$exception->getMessage());
+
+    // Votre requête SQL pour récupérer des recettes
+    $sqlQuery = 'SELECT * FROM recipes WHERE is_enabled = 1'; // Assurez-vous d'ajuster cette requête en fonction de vos besoins.
+
+    // Préparation de la requête
+    $recipesStatement = $db->prepare($sqlQuery);
+
+    // Exécution de la requête
+    $recipesStatement->execute();
+
+    // Récupération des résultats dans un tableau
+    $recipes = $recipesStatement->fetchAll();
+
+    // Vous pouvez maintenant utiliser le tableau $recipes pour afficher les données ou effectuer d'autres opérations.
+    foreach ($recipes as $recipe) {
+        echo '<p>' . $recipe['author'] . '</p>';
+    }
+} catch (Exception $exception) {
+    die('Erreur : ' . $exception->getMessage());
 }
 ?>
